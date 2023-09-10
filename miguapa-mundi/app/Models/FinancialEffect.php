8141575f0300734e616f01e07d0a8b5d1bf956c0
Miguel Angel Calvache Giraldo <calvache116@gmail.com>
2023-09-10 16:22:32 -0500

    update: search by country name in search and fix a bug in the paginate

--- miguapa-mundi/app/Models/FinancialEffect.php
+++ miguapa-mundi/app/Models/FinancialEffect.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Collection;
 
 class FinancialEffect extends model
 {
@@ -13,6 +14,8 @@ class FinancialEffect extends model
      * $this->attributes['financial_effect] - int - contains the financial effect of the news
      * $this->attributes['news_id] - int - contains the financial effect of the news
      * $this->news - contains the news that the financial effect is related to
+     * $this->attributes['country_id] - int - contains the financial effect of the news
+     * $this->countries - contains the countries that the financial effect is related to
      * $this->attributes['created_at'] - created_at - when the product was created
      * $this->attributes['updated_at] - updated_at - when the product was updated
      */
@@ -32,14 +35,14 @@ class FinancialEffect extends model
         $this->attributes['news_id'] = $news_id;
     }
 
-    public function getFinancialEffect(): int
+    public function getEffect(): int
     {
-        return $this->attributes['financial_effect'];
+        return $this->attributes['effect'];
     }
 
-    public function setFinancialEffect(int $financial_effect): void
+    public function setEffect(int $effect): void
     {
-        $this->attributes['financial_effect'] = $financial_effect;
+        $this->attributes['effect'] = $effect;
     }
 
     public function news(): BelongsTo
@@ -55,6 +58,31 @@ class FinancialEffect extends model
     public function setNews(News $news): void
     {
         $this->news = $news;
+    }
+
+    public function getCountryId(): int
+    {
+        return $this->attributes['country_id'];
+    }
+
+    public function setCountryId(int $country_id): void
+    {
+        $this->attributes['country_id'] = $country_id;
+    }
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function getCountries(): Country
+    {
+        return $this->country;
+    }
+
+    public function setCountries(Country $country): void
+    {
+        $this->country = $country;
     }
 
     public function getCreatedAt(): string

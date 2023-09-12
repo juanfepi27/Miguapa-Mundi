092b006455e42281298e097d7ac574c38238f1f6
juanfepi27 <juanfepi27@gmail.com>
2023-09-12 18:59:31 -0500

    feature: adding authentication restrictions to the routes. adding admin restrictions to the admin panel

--- miguapa-mundi/routes/web.php
+++ miguapa-mundi/routes/web.php
@@ -14,21 +14,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
-Route::get('/news', 'App\Http\Controllers\NewsController@index')->name('news.index');
-Route::get('/news/search', 'App\Http\Controllers\NewsController@search')->name('news.search');
-Route::get('/news/show/{id}', 'App\Http\Controllers\NewsController@show')->name('news.show');
-Route::get('/countries', 'App\Http\Controllers\CountryController@index')->name('country.index');
-Route::get('/countries/create', 'App\Http\Controllers\CountryController@create')->name("country.create");
-Route::post('/countries/save', 'App\Http\Controllers\CountryController@save')->name("country.save");
-Route::get('/alliances', 'App\Http\Controllers\AllianceController@index')->name('alliance.index');
-Route::get('/alliances/create', 'App\Http\Controllers\AllianceController@create')->name("alliance.create");
-Route::post('/alliances/save', 'App\Http\Controllers\AllianceController@save')->name("alliance.save");
-Route::get('/offer/to-me', 'App\Http\Controllers\OfferController@toMe')->name('offer.toMe');
-Route::get('/offer/by-me', 'App\Http\Controllers\OfferController@byMe')->name('offer.byMe');
-Route::get('/offer/create', 'App\Http\Controllers\OfferController@create')->name('offer.create');
-Route::get('/offer/delete/{id}', 'App\Http\Controllers\OfferController@delete')->name('offer.delete');
-Route::get('/offer/accept/{id}', 'App\Http\Controllers\OfferController@accept')->name('offer.accept');
-Route::get('/offer/reject/{id}', 'App\Http\Controllers\OfferController@reject')->name('offer.reject');
-Route::post('/offer/save', 'App\Http\Controllers\OfferController@save')->name('offer.save');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/news', 'App\Http\Controllers\NewsController@index')->name('news.index');
+    Route::get('/news/search', 'App\Http\Controllers\NewsController@search')->name('news.search');
+    Route::get('/news/show/{id}', 'App\Http\Controllers\NewsController@show')->name('news.show');
+    Route::get('/countries', 'App\Http\Controllers\CountryController@index')->name('country.index');
+    Route::get('/countries/create', 'App\Http\Controllers\CountryController@create')->name("country.create");
+    Route::post('/countries/save', 'App\Http\Controllers\CountryController@save')->name("country.save");
+    Route::get('/alliances', 'App\Http\Controllers\AllianceController@index')->name('alliance.index');
+    Route::get('/alliances/create', 'App\Http\Controllers\AllianceController@create')->name("alliance.create");
+    Route::post('/alliances/save', 'App\Http\Controllers\AllianceController@save')->name("alliance.save");
+    Route::get('/offer/to-me', 'App\Http\Controllers\OfferController@toMe')->name('offer.toMe');
+    Route::get('/offer/by-me', 'App\Http\Controllers\OfferController@byMe')->name('offer.byMe');
+    Route::get('/offer/create', 'App\Http\Controllers\OfferController@create')->name('offer.create');
+    Route::get('/offer/delete/{id}', 'App\Http\Controllers\OfferController@delete')->name('offer.delete');
+    Route::get('/offer/accept/{id}', 'App\Http\Controllers\OfferController@accept')->name('offer.accept');
+    Route::get('/offer/reject/{id}', 'App\Http\Controllers\OfferController@reject')->name('offer.reject');
+    Route::post('/offer/save', 'App\Http\Controllers\OfferController@save')->name('offer.save');
+});
+
+Route::middleware(['auth', 'role'])->group(function () {
+    // Aquí defines tus rutas que requieren autenticación y el rol "admin"
+});
 
 Auth::routes();

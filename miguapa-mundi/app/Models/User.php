5140ed29e2528bec633ad556797c96b28ad2f70d
juanfepi27 <juanfepi27@gmail.com>
2023-09-11 22:56:35 -0500

    other: rename getters and setters for username

--- miguapa-mundi/app/Models/User.php
+++ miguapa-mundi/app/Models/User.php
@@ -71,12 +71,12 @@ class User extends Authenticatable
         $this->attributes['name'] = $name;
     }
 
-    public function getUserName(): string
+    public function getUsername(): string
     {
         return $this->attributes['username'];
     }
 
-    public function setUserName(string $username): void
+    public function setUsername(string $username): void
     {
         $this->attributes['username'] = $username;
     }

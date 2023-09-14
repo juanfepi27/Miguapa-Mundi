cf29dee6819c7757a8fbd6763f04d5caa1a4b08e
Mapa <mpayalal@eafit.edu.co>
2023-09-14 08:25:02 -0500

    update: Applied lang in country index

--- miguapa-mundi/resources/lang/en/country.php
+++ miguapa-mundi/resources/lang/en/country.php
@@ -1,7 +1,15 @@
 <?php
 
 return [
-    'TITLE' => [
-        'KEY'=>'VALUE',
+    'index' => [
+        'titleTemplate'=>"Country Page - Miguapa Mundi",
+        'altImage'=>"Country's flag",
+        'cardOwner'=>'Owner', 
+        'cardMinimumOfferValue'=>'Minimum offer value', 
+        'cardAttractiveValue'=>'Attractive value', 
+        'cardIsInOffer'=>'The country is in offer', 
+        'cardIsNotInOffer'=>"The country isn't in offer",
+        'cardAlliances'=>'Alliances',
+        'btnSendOffer'=>'Send offer',
     ],
 ];
--- miguapa-mundi/resources/views/country/index.blade.php
+++ miguapa-mundi/resources/views/country/index.blade.php
@@ -7,21 +7,21 @@
             <div class="col-md-4 col-lg-3 mb-2">
                 <div class="card mt-3">
                     <div class="card">
-                        <img class="card-img-top" src="{{ asset('storage/' . $country->getFlag()) }}" alt="Country's flag">
+                        <img class="card-img-top" src="{{ asset('storage/' . $country->getFlag()) }}" alt="@lang('country.index.altImage')">
                         <div class="card-body">
                             <h5 class="card-title text-center" style="color: {{ $country->getColor() }}; text-shadow: 1px 1px 2px black;">{{ $country->getName() }}</h5>
                             <p class="card-text text-center"><em>{{ $country->getNickName() }}</em></p>
                             <br>
-                            <p class="card-text">Owner: {{ $country->getUserOwner()->getUsername() }}</p>
-                            <p class="card-text">Minimum offer value: ${{ $country->getMinimumOfferValue() }}</p>
-                            <p class="card-text">Attractive value: {{ $country->getAttractiveValue() }}</p>
+                            <p class="card-text">@lang('country.index.cardOwner'): {{ $country->getUserOwner()->getUsername() }}</p>
+                            <p class="card-text">@lang('country.index.cardMinimumOfferValue'): ${{ $country->getMinimumOfferValue() }}</p>
+                            <p class="card-text">@lang('country.index.cardAttractiveValue'): {{ $country->getAttractiveValue() }}</p>
                             @if ($country->getInOffer())
-                                <p>The country <strong>is</strong> in offer</p>
+                                <p>@lang('country.index.cardIsInOffer')</p>
                             @else
-                                <p>The country <strong>isn't</strong> in offer</p>
+                                <p>@lang('country.index.cardIsNotInOffer')</p>
                             @endif
                             @if ( $country->getMembers()->isNotEmpty() )
-                            <p class="card-text">Alliances:</p>
+                            <p class="card-text">@lang('country.index.cardAlliances'):</p>
                             <ul>
                                 @foreach ( $country->getMembers() as $member )
                                     <li>{{ $member->getAlliance()->getName() }}</li>
@@ -32,7 +32,7 @@
                             @else
                             <form class="text-center" action="#" method="POST">
                                 @csrf
-                                <button type="submit" class="btn btn-primary">Send offer</button>
+                                <button type="submit" class="btn btn-primary">@lang('country.index.btnSendOffer')</button>
                             </form>
                            @endguest
                         </div>

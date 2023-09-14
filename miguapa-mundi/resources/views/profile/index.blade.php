cd224e0563be0c77106f309e2765eedd97b6bb90
juanfepi27 <juanfepi27@gmail.com>
2023-09-14 07:06:09 -0500

    feature: adding lang folders and implementing it at profile

--- miguapa-mundi/resources/views/profile/index.blade.php
+++ miguapa-mundi/resources/views/profile/index.blade.php
@@ -4,23 +4,23 @@
 <div class="container w-75">
     <div class="card mt-3">
         <div class="card-header text-center bg-secondary">
-            <h5 class="card-title">MY PROFILE</h5>
+            <h5 class="card-title">@lang('profile.index.cardTitle')</h5>
         </div>
         <div class="card-body">
-            <p class="card-text"><strong>Name: </strong>{{ $viewData['user']->getName() }}</p>
-            <p class="card-text"><strong>Username: </strong>{{ $viewData['user']->getUsername() }}</p>
-            <p class="card-text"><strong>Email: </strong>{{ $viewData['user']->getEmail() }}</p>
-            <p class="card-text"><strong>Nationality: </strong>{{ $viewData['user']->getNationality() }}</p>
-            <p class="card-text"><strong>Budget: </strong>$ {{ $viewData['user']->getBudgetFormatted() }}</p>
-            <p class="card-text fw-bold">My countries:</p>
+            <p class="card-text"><strong>@lang('profile.index.formName'): </strong>{{ $viewData['user']->getName() }}</p>
+            <p class="card-text"><strong>@lang('profile.index.formUsername'): </strong>{{ $viewData['user']->getUsername() }}</p>
+            <p class="card-text"><strong>@lang('profile.index.formEmil'): </strong>{{ $viewData['user']->getEmail() }}</p>
+            <p class="card-text"><strong>@lang('profile.index.formNationality'): </strong>{{ $viewData['user']->getNationality() }}</p>
+            <p class="card-text"><strong>@lang('profile.index.formBudget'): </strong>$ {{ $viewData['user']->getBudgetFormatted() }}</p>
+            <p class="card-text fw-bold">@lang('profile.index.formCountries'):</p>
             <ul class="list-group list-group-flush">
                 @foreach ($viewData['user']->getBoughtCountries() as $country)
                     <li class="list-group-item"><i class="fa fa-arrow-right"></i>   {{ $country->getName() }} </li>
                 @endforeach
             </ul>
             <div class="d-flex justify-content-evenly gap-3">
-                <a href="{{ route('profile.addBudget') }}" class="btn btn-primary">Add budget</a>
-                <a href="#" class="btn btn-secondary">Change password</a>
+                <a href="{{ route('profile.addBudget') }}" class="btn btn-primary">@lang('profile.index.btnAdd')</a>
+                <a href="#" class="btn btn-secondary">@lang('profile.index.btnChange')</a>
             </div>
         </div>
     </div>

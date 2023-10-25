ec41a13836299bf3866bbf334841ac953f962c47
juanfepi27 <juanfepi27@gmail.com>
2023-10-25 09:56:09 -0500

    feature: middleware to delete, accept or reject the offers

--- miguapa-mundi/routes/web.php
+++ miguapa-mundi/routes/web.php
@@ -30,9 +30,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/offer/to-me', 'App\Http\Controllers\OfferController@toMe')->name('offer.toMe');
     Route::get('/offer/by-me', 'App\Http\Controllers\OfferController@byMe')->name('offer.byMe');
     Route::get('/offer/create', 'App\Http\Controllers\OfferController@create')->name('offer.create');
-    Route::get('/offer/delete/{id}', 'App\Http\Controllers\OfferController@delete')->name('offer.delete');
-    Route::get('/offer/accept/{id}', 'App\Http\Controllers\OfferController@accept')->name('offer.accept');
-    Route::get('/offer/reject/{id}', 'App\Http\Controllers\OfferController@reject')->name('offer.reject');
+    Route::middleware('myOffer')->group(function () {
+        Route::get('/offer/delete/{id}', 'App\Http\Controllers\OfferController@delete')->name('offer.delete');
+    });
+    Route::middleware('offerToMe')->group(function () {
+        Route::get('/offer/accept/{id}', 'App\Http\Controllers\OfferController@accept')->name('offer.accept');
+        Route::get('/offer/reject/{id}', 'App\Http\Controllers\OfferController@reject')->name('offer.reject');
+    });
     Route::post('/offer/save', 'App\Http\Controllers\OfferController@save')->name('offer.save');
     Route::get('/profile', 'App\Http\Controllers\ProfileController@index')->name('profile.index');
     Route::get('/profile/addBudget', 'App\Http\Controllers\ProfileController@addBudget')->name('profile.addBudget');
